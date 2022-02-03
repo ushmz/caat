0d232d3063d0b22487d4160da1ea203e2abf2ade
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2022-02-03 22:32:31 +0900

    :clipboard: copy

--- background.js
+++ background.js
@@ -1,36 +1,43 @@
+const copyTextToClipboard = (val) => {
+  const cpForm = document.createElement('textarea');
+  cpForm.textContent = val;
+  const body = document.getElementsByTagName('body')[0];
+  body.appendChild(cpForm);
+  cpForm.select();
+  const ok = document.execCommand('copy');
+  body.removeChild(cpForm);
+  return ok;
+};
+
 chrome.contextMenus.create({
-  title: "Markdown形式でコピー",
-  type: "normal",
-  contexts: ["all"],
+  title: 'Markdown形式でコピー',
+  type: 'normal',
+  contexts: ['all'],
   onclick: (info, tab) => {
     if (info.selectionText) {
-      console.log(
-        `[${info.selectionText.replace(/\s*[\[\]]\s*/g, " ")}](${info.linkUrl})`
-      );
+      copyTextToClipboard(`[${info.selectionText.replace(/\s*[\[\]]\s*/g, ' ')}](${info.linkUrl})`);
     } else {
       if (!info.linkUrl) {
-        console.log(`[${tab.title.replace(/\s*[\[\]]\s*/g, " ")}](${tab.url})`);
+        copyTextToClipboard(`[${tab.title.replace(/\s*[\[\]]\s*/g, ' ')}](${tab.url})`);
       } else {
-        console.log(`[](${info.linkUrl})`);
+        copyTextToClipboard(`[](${info.linkUrl})`);
       }
     }
   },
 });
 
 chrome.contextMenus.create({
-  title: "ScrapBox形式でコピー",
-  type: "normal",
-  contexts: ["all"],
+  title: 'ScrapBox形式でコピー',
+  type: 'normal',
+  contexts: ['all'],
   onclick: (info, tab) => {
     if (info.selectionText) {
-      console.log(
-        `[${info.selectionText.replace(/\s*[\[\]]\s*/g, " ")} ${info.linkUrl}]`
-      );
+      copyTextToClipboard(`[${info.selectionText.replace(/\s*[\[\]]\s*/g, ' ')} ${info.linkUrl}]`);
     } else {
       if (!info.linkUrl) {
-        console.log(`[${tab.title.replace(/\s*[\[\]]\s*/g, " ")} ${tab.url}]`);
+        copyTextToClipboard(`[${tab.title.replace(/\s*[\[\]]\s*/g, ' ')} ${tab.url}]`);
       }
-      console.log(`[ ${info.linkUrl}]`);
+      copyTextToClipboard(`[ ${info.linkUrl}]`);
     }
   },
 });
