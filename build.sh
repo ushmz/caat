6b692d02827fb670f1001b0216dbd16012a59070
ushmz <44394399+ushmz@users.noreply.github.com>
2024-01-09 22:55:57 +0900

    feat: update permission (#7)
    * feat: update permission
    
    * feat: add build script
    
    * chore: version

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+zip caat.zip \
+	./background.js \
+	./manifest.json \
+	./icon_16x16.png \
+	./icon_48x48.png \
+	./icon_128x128.png \
+	./_locales/en/messages.json \
+	./_locales/ja/messages.json
