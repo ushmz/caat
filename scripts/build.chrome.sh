741ecf014449ae3b0ce4708a35db5ae1c25a1f82
ushmz <44394399+ushmz@users.noreply.github.com>
2024-11-10 18:58:07 +0900

    chore(script): fix output directory

--- scripts/build.chrome.sh
+++ scripts/build.chrome.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+mkdir -p ./artifacts/chrome
 cp ./manifest/base.json ./manifest.json
 zip caat.zip \
 	./background.js \
@@ -9,4 +10,5 @@ zip caat.zip \
 	./icon_128x128.png \
 	./_locales/en/messages.json \
 	./_locales/ja/messages.json
+mv caat.zip ./artifacts/chrome/caat.zip
 rm ./manifest.json
